c6c38f5fe3e942be2fddaeaec0622e09424c6d9f
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-09-05 19:49:56 +0300

    Added migration identity for the db.

--- CoreApp/src/CoreApp/Migrations/20170905164017_Identity.Designer.cs
+++ CoreApp/src/CoreApp/Migrations/20170905164017_Identity.Designer.cs
@@ -8,9 +8,10 @@ using CoreApp.Entities;
 namespace CoreApp.Migrations
 {
     [DbContext(typeof(FoodDbContext))]
-    partial class FoodDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170905164017_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
