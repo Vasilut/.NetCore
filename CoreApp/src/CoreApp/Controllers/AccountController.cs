4bc722ff529138daed9c0fe2bc9f22400685ca72
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-09-09 20:23:32 +0300

    Added register feature.

---
+++ CoreApp/src/CoreApp/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using CoreApp.Entities;
+using CoreApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+                var createResult = await _userManager.CreateAsync(user, model.Password);
+                if(createResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+
+            }
+            return View();
+        }
+    }
+}
