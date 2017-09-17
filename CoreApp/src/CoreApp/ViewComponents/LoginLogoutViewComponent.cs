bc283d9772c1f2b28ebcaa10cf980dead1a5e9d5
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-09-17 18:26:36 +0300

    Added Login and Logout functionallity.

---
+++ CoreApp/src/CoreApp/ViewComponents/LoginLogoutViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApp.ViewComponents
+{
+    public class LoginLogoutViewComponent: ViewComponent
+    {
+        public LoginLogoutViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
