751be35ab8b2a7dda330f17a9cface0be3236246
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-10-22 22:32:03 +0300

    Initial Commit

---
+++ DartuContestHosted/src/DartuContestHosted/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DartuContestHosted.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            ViewData["Message"] = "Result Page.";
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
