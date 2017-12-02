6fa06ef079bcf34c326e4cf7863edbf64a1cb160
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-12-02 13:38:23 +0200

    Authorize the ResultController.

--- DartuContestHosted/src/DartuContestHosted/Controllers/ResultController.cs
+++ DartuContestHosted/src/DartuContestHosted/Controllers/ResultController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DartuContestHosted.Models;
 using DartuContestHosted.Services;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DartuContestHosted.Controllers
 {
+    [Authorize]
     public class ResultController : Controller
     {
         private IParticipantResultsRepository _participantResultsRepository;
