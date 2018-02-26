709af3fbd5df87b921539078b7b3716c90f4ce3b
Vasilut <lucian.vasilut10@gmail.com>
2018-02-26 14:44:02 +0200

    Added web api in .net core 2.0 + postgreSql.

---
+++ WebApiMock/Domain/Profesor.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Disciplina { get; set; }
+    }
+}
